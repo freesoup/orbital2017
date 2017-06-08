31d940831c1586536e7b23b072678ab3c6fe770b
Kennard Ng <Kennard_ng@outlook.com>
2017-06-08 16:59:54 +0800

    added basic drag functionality

--- client/ui/semester.js
+++ client/ui/semester.js
@@ -1,3 +1,14 @@
 import { Template } from 'meteor/templating';
 import { ReactiveVar } from 'meteor/reactive-var';
 import './semester.html';
+
+Template.module.events({
+    'click .module'(event) {
+        const target = event.target;
+        $(target).simulate('drag');
+    },
+});
+
+Template.sem_template.onRendered(function() {
+    $('.module').draggable();
+});
