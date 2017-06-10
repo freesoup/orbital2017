29771b5fd456894130a28ad56c44e42cffdf0544
Kennard Ng <Kennard_ng@outlook.com>
2017-06-10 13:41:45 +0800

    added comments and clean up some stuff in the .js of semester

--- client/ui/semester.js
+++ client/ui/semester.js
@@ -2,13 +2,6 @@ import { Template } from 'meteor/templating';
 import { ReactiveVar } from 'meteor/reactive-var';
 import './semester.html';
 
-Template.module.events({
-    'click .module'(event) {
-        const target = event.target;
-        $(target).simulate('drag');
-    },
-});
-
 Template.sem_template.onRendered(function() {
     $('.module').draggable();
     $('.semester').droppable();
